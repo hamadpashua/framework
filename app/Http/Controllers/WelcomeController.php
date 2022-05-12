2cf520f5b1d5c5cb566a32c873f74bf2a25dc83a
hamadpashua <102689093+hamadpashua@users.noreply.github.com>
2022-05-12 16:18:31 +0300

    [#6] sixth commit

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -10,30 +10,8 @@ class WelcomeController extends Controller
 {
     
     public function index()
-    {
-        if (Request::has('post')) {
-            $request = Request::get('post');
-            $user = User::where('name', $request->name)
-                    ->orWhere('name', $request->name)
-                    ->first();
-            if ($user) {
-                Session::set('SESSION_USER', $user->name);
-                Session::set('SESSION_UID', $user->id);
-                Session::set('success', 'Your successfull authorized');
-                return redirect('/test');
-            } else {
-                Session::set('fail', 'User not found');
-                return view('welcome');
-            }
-        }
-
-        
-        return view('welcome');
-    }
-
-    public function formT()
-    {
-        return view('welcome');
+    { 
+        return view('index');
     }
 
     public function logout()
